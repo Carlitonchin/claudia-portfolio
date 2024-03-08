67a04313fe2fae5aa0a6bb9a840657ecfb777c28
CarlosArrieta <carrieta@defontana.com>
2024-03-08 19:10:38 -0300

    add some changes

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+import animations from "@midudev/tailwind-animations";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#f6f7fa",
+        "font-normal": "#616178",
+        "font-title": "#1c1c2c",
+        primary: "#3aa0ff",
+      },
+    },
+  },
+  plugins: [animations],
+};
